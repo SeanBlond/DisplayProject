15776cc0225a989553053818cdefd8b6a6f618fb
SeanA <sean.ammon@mymail.champlain.edu>
2026-08-19 00:47:27 -0400

    Changed display to be vertical
    Redid a good chunk of the weather graph displaying, also made it into a function that can be called to draw at various y-positions

--- testDisplay.py
+++ testDisplay.py
@@ -11,207 +11,217 @@ try:
 except:
     print("Failed to load Inky library")
 
-# Defining information for requesting data
-BASE_URL = "https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/hourly"
-HEADERS = {
-    "apikey" : open('api_key', 'r').read(),
-    "accept" : "applications"
-}
-PARAMS = {
-    "latitude": "56.462002",
-    "longitude": "-2.970700",
-    "excludeParameterMetadata": "true",
-    "includeLocationName": "true"
-}
-
-# Arrays for weather codes
-WEATHER_CODE_DESCRIPTIONS = {
-    0:  "Clear Night", 
-    1:  "Sunny Day",
-    2:  "Partly Cloudy (night)",
-    3:  "Partly Cloudy (day)",
-    4:  "Not Used",
-    5:  "Mist",
-    6:  "Fog",
-    7:  "Cloudy",
-    8:	"Overcast",
-    9:	"Light rain shower (night)",
-    10:	"Light rain shower (day)",
-    11:	"Drizzle",
-    12:	"Light rain",
-    13:	"Heavy rain shower (night)",
-    14:	"Heavy rain shower (day)",
-    15:	"Heavy rain",
-    16:	"Sleet shower (night)",
-    17:	"Sleet shower (day)",
-    18:	"Sleet",
-    19:	"Hail shower (night)",
-    20:	"Hail shower (day)",
-    21:	"Hail",
-    22:	"Light snow shower (night)",
-    23:	"Light snow shower (day)",
-    24:	"Light snow",
-    25:	"Heavy snow shower (night)",
-    26:	"Heavy snow shower (day)",
-    27:	"Heavy snow",
-    28:	"Thunder shower (night)",
-    29:	"Thunder shower (day)",
-    30:	"Thunder"
-}
-WEATHER_CODE_SYMBOLS = {
-    0 :  "",  # Clear Night
-    1 :  "",  # Sunny Day
-    2 :  "",  # Partly Cloudy (night)
-    3 :  "",  # Partly Cloudy (day)
-    4 :  "",  # Not Used
-    5 :  "",  # Mist
-    6 :  "",  # Fog
-    7 :  "",  # Cloudy
-    8 :  "",  # Overcast
-    9 :  "",  # Light rain shower (night)
-    10 : "",  # Light rain shower (day)
-    11 : "",  # Drizzle
-    12 : "",  # Light rain
-    13 : "",  # Heavy rain shower (night)
-    14 : "",  # Heavy rain shower (day)
-    15 : "",  # Heavy rain
-    16 : "",  # Sleet shower (night)
-    17 : "",  # Sleet shower (day)
-    18 : "",  # Sleet
-    19 : "",  # Hail shower (night)
-    20 : "",  # Hail shower (day)
-    21 : "",  # Hail
-    22 : "",  # Light snow shower (night)
-    23 : "",  # Light snow shower (day)
-    24 : "",  # Light snow
-    25 : "",  # Heavy snow shower (night)
-    26 : "",  # Heavy snow shower (day)
-    27 : "",  # Heavy snow
-    28 : "",  # Thunder shower (night)
-    29 : "",  # Thunder shower (day)
-    30 : ""   # Thunder
-}
-
-# Defining the time series array that will contain the info for the display
-timeSeriesList = [];
-
-# Getting the info from the API
-try:
-    response = requests.get(BASE_URL, headers=HEADERS, params=PARAMS, timeout=10)
-
-    # Chefcking if the request was succesful
-    response.raise_for_status()
-
-    # Parsing the data
-    weather_data = response.json()
-    print("Succesfully read API data located at", weather_data["features"][0]["properties"]["location"]["name"])
-    timeSeriesList = weather_data["features"][0]["properties"]["timeSeries"];
-
-except requests.exceptions.requests.HTTPError as http_err:
-    print(f"HTTP error occurred: {http_err}")
-
-except Exception as err:
-    print(f"Error occurred: {err}")
-
-
-# Looping through each time if applicable
-if (len(timeSeriesList) == 0):
-    sys.exit(0)
-
-
 # Creating image and drawing device
-image = Image.new("P", (800, 480), "white")
+image = Image.new("P", (480, 800), "white")
 draw = ImageDraw.Draw(image)
 
-# Loading font
+# Loading fonts
 try:
-    lato_font_bold = ImageFont.truetype("Lato/Lato-Bold.ttf", size=20)
-    lato_font_regular = ImageFont.truetype("Lato/Lato-Regular.ttf", size=20)
-    symbol_font = ImageFont.truetype("easy_weather_icons_font/easy_weather_icons_font.ttf", size=40)
+    small_lato_font_regular = ImageFont.truetype("Lato/Lato-Regular.ttf", size=15)
+    symbol_font = ImageFont.truetype("easy_weather_icons_font/easy_weather_icons_font.ttf", size=35)
 except IOError:
-    lato_font_large = ImageFont.load_default()
-    lato_font_small = ImageFont.load_default()
+    small_lato_font_regular = ImageFont.load_default()
     symbol_font = ImageFont.load_default()
 
-# Getting the current date and allowed tomorrow date
-dundeeTime = datetime.now(ZoneInfo("Europe/London")) + timedelta(days=1)
-allowedFutureDate = (dundeeTime + timedelta(days=1)).replace(hour=0)
 
-# Defining members for keeping track of temp data for the graph
-maxGraphTemp = max(timeSeriesList[0]["screenTemperature"], timeSeriesList[0]["feelsLikeTemperature"])
-minGraphTemp = min(timeSeriesList[0]["screenTemperature"], timeSeriesList[0]["feelsLikeTemperature"])
-maxTemp = timeSeriesList[0]["screenTemperature"]
-minTemp = timeSeriesList[0]["screenTemperature"]
-actualTempPoints = []
-feelsLikeTempPoints = []
+def DrawWeatherGraph(startingYPos):
 
-# Looping through each time series data point
-index = 0;
-for entry in timeSeriesList:
-    # Getting time data
-    dateTimeObject = datetime.strptime(entry["time"], "%Y-%m-%dT%H:%S%z")
+    # Defining information for requesting data
+    BASE_URL = "https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/hourly"
+    HEADERS = {
+        "apikey" : open('api_key', 'r').read(),
+        "accept" : "applications"
+    }
+    PARAMS = {
+        "latitude": "56.462002",
+        "longitude": "-2.970700",
+        "excludeParameterMetadata": "true",
+        "includeLocationName": "true"
+    }
 
-    # If the date doesn't equal the current date, skip (except for allowed future date)
-    if (dateTimeObject.date() != dundeeTime.date() and 
-        (dateTimeObject.date() != allowedFutureDate.date() or
-        dateTimeObject.hour != allowedFutureDate.hour)):
-        continue
+    # Arrays for weather codes
+    WEATHER_CODE_DESCRIPTIONS = {
+        0:  "Clear Night", 
+        1:  "Sunny Day",
+        2:  "Partly Cloudy (night)",
+        3:  "Partly Cloudy (day)",
+        4:  "Not Used",
+        5:  "Mist",
+        6:  "Fog",
+        7:  "Cloudy",
+        8:	"Overcast",
+        9:	"Light rain shower (night)",
+        10:	"Light rain shower (day)",
+        11:	"Drizzle",
+        12:	"Light rain",
+        13:	"Heavy rain shower (night)",
+        14:	"Heavy rain shower (day)",
+        15:	"Heavy rain",
+        16:	"Sleet shower (night)",
+        17:	"Sleet shower (day)",
+        18:	"Sleet",
+        19:	"Hail shower (night)",
+        20:	"Hail shower (day)",
+        21:	"Hail",
+        22:	"Light snow shower (night)",
+        23:	"Light snow shower (day)",
+        24:	"Light snow",
+        25:	"Heavy snow shower (night)",
+        26:	"Heavy snow shower (day)",
+        27:	"Heavy snow",
+        28:	"Thunder shower (night)",
+        29:	"Thunder shower (day)",
+        30:	"Thunder"
+    }
+    WEATHER_CODE_SYMBOLS = {
+        0 :  "",  # Clear Night
+        1 :  "",  # Sunny Day
+        2 :  "",  # Partly Cloudy (night)
+        3 :  "",  # Partly Cloudy (day)
+        4 :  "",  # Not Used
+        5 :  "",  # Mist
+        6 :  "",  # Fog
+        7 :  "",  # Cloudy
+        8 :  "",  # Overcast
+        9 :  "",  # Light rain shower (night)
+        10 : "",  # Light rain shower (day)
+        11 : "",  # Drizzle
+        12 : "",  # Light rain
+        13 : "",  # Heavy rain shower (night)
+        14 : "",  # Heavy rain shower (day)
+        15 : "",  # Heavy rain
+        16 : "",  # Sleet shower (night)
+        17 : "",  # Sleet shower (day)
+        18 : "",  # Sleet
+        19 : "",  # Hail shower (night)
+        20 : "",  # Hail shower (day)
+        21 : "",  # Hail
+        22 : "",  # Light snow shower (night)
+        23 : "",  # Light snow shower (day)
+        24 : "",  # Light snow
+        25 : "",  # Heavy snow shower (night)
+        26 : "",  # Heavy snow shower (day)
+        27 : "",  # Heavy snow
+        28 : "",  # Thunder shower (night)
+        29 : "",  # Thunder shower (day)
+        30 : ""   # Thunder
+    }
 
-    # Calculating time
-    timeString = dateTimeObject.strftime("%#I%p").lower()
+    # Defining the time series array that will contain the info for the display
+    timeSeriesList = [];
 
-    # Only draw the data if the hour is even
-    if (dateTimeObject.hour % 2 == 0):
-        # Drawing weather condition symbol
-        draw.text(((index + 0.5) * 61.5, 450), WEATHER_CODE_SYMBOLS[entry["significantWeatherCode"]], fill=(0, 0, 0), font=symbol_font, anchor="ms")
+    # Getting the info from the API
+    try:
+        response = requests.get(BASE_URL, headers=HEADERS, params=PARAMS, timeout=10)
 
-        # Drawing time
-        draw.text(((index + 0.5) * 61.5, 470), timeString, fill=(0, 0, 0), font=lato_font_bold, anchor="ms")
+        # Chefcking if the request was succesful
+        response.raise_for_status()
 
-        # Increasing index
-        index += 1
+        # Parsing the data
+        weather_data = response.json()
+        print("Succesfully read API data located at", weather_data["features"][0]["properties"]["location"]["name"])
+        timeSeriesList = weather_data["features"][0]["properties"]["timeSeries"];
 
-    # Adding the temperature data points
-    actualTempPoints.append(entry["screenTemperature"])
-    feelsLikeTempPoints.append(entry["feelsLikeTemperature"])
+    except requests.exceptions.requests.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
 
-    # Updating max/min temps
-    maxGraphTemp = max(maxTemp, max(entry["screenTemperature"], entry["feelsLikeTemperature"]))
-    minGraphTemp = min(minTemp, min(entry["screenTemperature"], entry["feelsLikeTemperature"]))
-    maxTemp = max(maxTemp, entry["screenTemperature"])
-    minTemp = min(minTemp, entry["screenTemperature"])
+    except Exception as err:
+        print(f"Error occurred: {err}")
 
-# Drawing a box for the temp graph
-tempGraphHeight = max(maxGraphTemp - minGraphTemp, 5)
-draw.rectangle(
-    (30, 320, 770, 410),
-    fill=(200, 200, 200)
-)
-draw.text((25, 410), str(round(minGraphTemp)), fill=(0, 0, 0), font=lato_font_regular, anchor="rb")
-draw.text((25, 320), str(round(maxGraphTemp)), fill=(0, 0, 0), font=lato_font_regular, anchor="rt")
-draw.line((30, 410, 770, 410), fill=(0, 0, 0), width=2)
-draw.line((30, 320, 770, 320), fill=(0, 0, 0), width=2)
 
-# Looping through the temp data points and drawing a graph
-for i in range(len(actualTempPoints) - 1):
-    # Calculating the line coords for the actual temperature
-    actualTempLineStart = (i * 30.833 + 30.75, (actualTempPoints[i] - minGraphTemp) / tempGraphHeight * -80 + 405)
-    actualTempLineEnd = ((i + 1) * 30.833 + 30.75, (actualTempPoints[i + 1] - minGraphTemp) / tempGraphHeight * -80 + 405)
+    # Looping through each time if applicable
+    if (len(timeSeriesList) == 0):
+        return
 
-    # Calculating the line coords for the feels like temperature
-    feelsLikeTempLineStart = (i * 30.833 + 30.75, (feelsLikeTempPoints[i] - minGraphTemp) / tempGraphHeight * -80 + 405)
-    feelsLikeTempLineEnd = ((i + 1) * 30.833 + 30.75, (feelsLikeTempPoints[i + 1] - minGraphTemp) / tempGraphHeight * -80 + 405)
+    # Drawing the background
+    draw.rectangle(
+        (0, startingYPos - 200, 480, startingYPos),
+        fill=(99, 151, 235))
 
-    # Drawing a line from point i to i + 1
-    draw.line((feelsLikeTempLineStart, feelsLikeTempLineEnd), fill=(100, 100, 100), width=3)
-    draw.line((actualTempLineStart, actualTempLineEnd), fill=(0, 0, 0), width=3)
+    # Getting the current date and allowed tomorrow date
+    dundeeTime = datetime.now(ZoneInfo("Europe/London")) + timedelta(days=1)
+    allowedFutureDate = (dundeeTime + timedelta(days=1)).replace(hour=0)
 
-# Drawing incremental lines for each integer degree value
-degrees = int(round(maxGraphTemp) - round(minGraphTemp))
-degreeOffset = 90 / float(degrees)
-for i in range(degrees):
-    lineYPos = 410 - (i * degreeOffset)
-    draw.line((30, lineYPos, 770, lineYPos), fill=(85, 85, 85), width=1)
+    # Defining members for keeping track of temp data for the graph
+    maxGraphTemp = max(timeSeriesList[0]["screenTemperature"], timeSeriesList[0]["feelsLikeTemperature"])
+    minGraphTemp = min(timeSeriesList[0]["screenTemperature"], timeSeriesList[0]["feelsLikeTemperature"])
+    maxTemp = timeSeriesList[0]["screenTemperature"]
+    minTemp = timeSeriesList[0]["screenTemperature"]
+    actualTempPoints = []
+    feelsLikeTempPoints = []
+
+    # Looping through each time series data point
+    index = 0;
+    for entry in timeSeriesList:
+        # Getting time data
+        dateTimeObject = datetime.strptime(entry["time"], "%Y-%m-%dT%H:%S%z")
+
+        # If the date doesn't equal the current date, skip (except for allowed future date)
+        if (dateTimeObject.date() != dundeeTime.date() and 
+            (dateTimeObject.date() != allowedFutureDate.date() or
+            dateTimeObject.hour != allowedFutureDate.hour)):
+            continue
+
+        # Calculating time
+        timeString = dateTimeObject.strftime("%#H").lower()
+
+        # Only draw the data if the hour is even
+        if (dateTimeObject.hour % 2 == 0):
+            # Drawing weather condition symbol
+            draw.text(((index + 0.5) * 37, startingYPos - 20), WEATHER_CODE_SYMBOLS[entry["significantWeatherCode"]], fill=(255, 255, 255), font=symbol_font, anchor="ms")
+
+            # Drawing time
+            draw.text(((index + 0.5) * 37, startingYPos - 5), timeString, fill=(255, 255, 255), font=small_lato_font_regular, anchor="ms")
+
+            # Increasing index
+            index += 1
+
+        # Adding the temperature data points
+        actualTempPoints.append(entry["screenTemperature"])
+        feelsLikeTempPoints.append(entry["feelsLikeTemperature"])
+
+        # Updating max/min temps
+        maxGraphTemp = max(maxTemp, max(entry["screenTemperature"], entry["feelsLikeTemperature"]))
+        minGraphTemp = min(minTemp, min(entry["screenTemperature"], entry["feelsLikeTemperature"]))
+        maxTemp = max(maxTemp, entry["screenTemperature"])
+        minTemp = min(minTemp, entry["screenTemperature"])
+
+    # Drawing a box for the temp graph
+    tempGraphHeight = max(maxGraphTemp - minGraphTemp, 5)
+    draw.rectangle(
+        (20, startingYPos - 190, 460, startingYPos - 60),
+        fill=(80, 118, 212)
+    )
+
+    # Drawing incremental lines for each integer degree value
+    degrees = int(round(maxGraphTemp) - round(minGraphTemp))
+    degreeOffset = 130 / float(degrees)
+    for i in range(degrees):
+        lineYPos = startingYPos - 60 - (i * degreeOffset)
+        draw.line((20, lineYPos, 460, lineYPos), fill=(37, 73, 161), width=1)
+
+    # Drawing lines and labels for the top and bottom lines
+    draw.text((18, startingYPos - 60), str(round(minGraphTemp)), fill=(255, 255, 255), font=small_lato_font_regular, anchor="rb")
+    draw.text((18, startingYPos - 190), str(round(maxGraphTemp)), fill=(255, 255, 255), font=small_lato_font_regular, anchor="rt")
+    draw.line((20, startingYPos - 60, 460, startingYPos - 60), fill=(255, 255, 255), width=2)
+    draw.line((20, startingYPos - 190, 460, startingYPos - 190), fill=(255, 255, 255), width=2)
+
+    # Looping through the temp data points and drawing a graph
+    for i in range(len(actualTempPoints) - 1):
+        # Calculating the line coords for the actual temperature
+        actualTempLineStart = (i * 18.333 + 20, (actualTempPoints[i] - minGraphTemp) / tempGraphHeight * -120 + startingYPos - 65)
+        actualTempLineEnd = ((i + 1) * 18.333 + 20, (actualTempPoints[i + 1] - minGraphTemp) / tempGraphHeight * -120 + startingYPos - 65)
+
+        # Calculating the line coords for the feels like temperature
+        feelsLikeTempLineStart = (i * 18.333 + 20, (feelsLikeTempPoints[i] - minGraphTemp) / tempGraphHeight * -120 + startingYPos - 65)
+        feelsLikeTempLineEnd = ((i + 1) * 18.333 + 20, (feelsLikeTempPoints[i + 1] - minGraphTemp) / tempGraphHeight * -120 + startingYPos - 65)
+
+        # Drawing a line from point i to i + 1
+        draw.line((feelsLikeTempLineStart, feelsLikeTempLineEnd), fill=(200, 200, 200), width=3)
+        draw.line((actualTempLineStart, actualTempLineEnd), fill=(255, 255, 255), width=3)
+
+# Drawing the weather graph
+DrawWeatherGraph(800)
 
 # Saving the image
 image.save("testImage.png")
